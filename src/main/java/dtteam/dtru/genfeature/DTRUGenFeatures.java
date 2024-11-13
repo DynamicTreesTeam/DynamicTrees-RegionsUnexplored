30ca6e81f921e0a0e8fab44efe51c920b3d0fd82
Max Hyper <massimocantugarcia@gmail.com>
2024-11-12 22:13:23 -0300

    redwoods

--- src/main/java/dtteam/dtru/genfeature/DTRUGenFeatures.java
+++ src/main/java/dtteam/dtru/genfeature/DTRUGenFeatures.java
@@ -12,6 +12,7 @@ public class DTRUGenFeatures {
     public static final GenFeature EXTRA_BOTTOM_FLARE = new ExtraBottomFlareGenFeature(new ResourceLocation(DynamicTreesRU.MOD_ID, "extra_bottom_flare"));
     public static final GenFeature REPLACE_ON_RADIUS = new ReplaceOnRadiusGenFeature(new ResourceLocation(DynamicTreesRU.MOD_ID, "replace_on_radius"));
     public static final GenFeature BIOME_PREDICATE_2 = new BiomePredicateGenFeature(new ResourceLocation(DynamicTreesRU.MOD_ID, "biome_predicate"));
+    public static final GenFeature SOIL_PREDICATE = new DontGenerateOnSoilGenFeature(new ResourceLocation(DynamicTreesRU.MOD_ID, "soil_predicate"));
     public static final GenFeature SYTHIAN_TOPPER = new SythianTopperGenFeature(new ResourceLocation(DynamicTreesRU.MOD_ID, "sythian_topper"));
     public static final GenFeature ALTERNATIVE_BRANCH = new AlternativeBranchGenFeature(new ResourceLocation(DynamicTreesRU.MOD_ID, "alt_branch"));
     public static final GenFeature MUSHROOM_VINES = new MushroomVinesGenFeature(new ResourceLocation(DynamicTreesRU.MOD_ID, "mushroom_vines"));
@@ -19,7 +20,7 @@ public class DTRUGenFeatures {
     public static void register(final Registry<GenFeature> registry) {
         registry.registerAll(BIG_BOTTOM_FLARE, EXTRA_BOTTOM_FLARE,
                 REPLACE_ON_RADIUS,
-                BIOME_PREDICATE_2,
+                BIOME_PREDICATE_2, SOIL_PREDICATE,
                 SYTHIAN_TOPPER, ALTERNATIVE_BRANCH, MUSHROOM_VINES);
     }
 
